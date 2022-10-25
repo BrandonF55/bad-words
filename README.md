35bc82040b06ba71fed7631aa6c89f9461e02d04
brandonf55 <you@example.com>
2022-10-25 11:44:57 -0700

    updated problem solving

--- README.md
+++ README.md
@@ -11,3 +11,27 @@ Code:
 const text = "hello there";
 wordCounter(text);
 Expected Output: 2
+
+
+
+
+Test: "It should show you all my three words"
+Code: wordCounter("hi there brandon");
+Expected Output: 3
+
+
+Describe: numberOfOccurrencesInText()
+
+Test: "It should return 0 occurrences of a word for an empty string."
+Code:
+const text = "";
+const word = "red";
+numberOfOccurrencesInText(word, text);
+Expected Output: 0
+
+
+test: 'outputs a single word when you put one in it"
+code:numberOfOccurrencesInText
+let wordCount
+numberOfOccurrencesInText(word, text)
+expeped 1
--- js/scripts.js
+++ js/scripts.js
@@ -13,5 +13,45 @@ function wordCounter(text) {
 		
   });
   return wordCount;
-}wordCounter;
-wordCounter('hi there im starting to get it');
+}
+
+function numberOfOccurrencesInText(word, text) {
+  if (word === text) {
+    return 1;
+  }
+  return 0;
+}
+
+function numberOfOccurrencesInText(word, text) {
+  const textArray = text.split(" ");
+  let wordCount = 0;
+  textArray.forEach(function(element) {
+    if (word.toLowerCase() === element.toLowerCase()) {
+      wordCount++
+    }
+		
+  }); 
+  return wordCount;
+} 
+numberOfOccurrencesInText('how','how mant words is this'); 
+
+
+function includesRarestLetter(word) {
+  if (word.toLowerCase().includes("q")) {
+    return true;
+  }
+  return false;
+}
+console.log(includesRarestLetter("queen"));
+
+function numberOfOccurrencesInText(word, text) {
+  const textArray = text.split(" ");
+  let wordCount = 0;
+  textArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+console.log(numberOfOccurrencesInText("epic", "epicodus"));
